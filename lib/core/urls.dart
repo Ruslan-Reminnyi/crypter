53a8698bc59e42dd601492d06d4eb1018e0aa272
Ruslan Reminnyi <reminny.r.v@gmail.com>
2026-05-27 17:25:22 +0300

    listening a newest candle info through a web socket and updating it in real time

--- lib/core/urls.dart
+++ lib/core/urls.dart
@@ -1,2 +1,3 @@
 const String binanceBaseUrl = 'https://api.binance.com';
 const String binanceCryptoInfoUrl = '$binanceBaseUrl/api/v3/klines';
+const String binanceBaseWebSocketUrl = 'wss://stream.binance.com:9443/ws';
--- lib/presentation/features/chart/chart_screen.dart
+++ lib/presentation/features/chart/chart_screen.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 
 import 'package:candlesticks/candlesticks.dart';
+import 'package:crypter/core/urls.dart';
 import 'package:crypter/data/services/crypto_info_service_impl.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 class ChartScreen extends StatefulWidget {
   const ChartScreen({super.key, required this.title});
@@ -15,10 +17,26 @@ class ChartScreen extends StatefulWidget {
 }
 
 class _ChartScreenState extends State<ChartScreen> {
-  List<Candle> _candles = [];
+  final List<Candle> _candles = [];
+  WebSocketChannel? _webSocketChannel;
+
+  @override
+  void initState() {
+    _webSocketChannel = WebSocketChannel.connect(Uri.parse(binanceBaseWebSocketUrl));
+    _webSocketChannel?.sink.add(
+      jsonEncode({
+        "method": "SUBSCRIBE",
+        "params": ['ethusdt@kline_1d'],
+        "id": 1,
+      }),
+    );
+    super.initState();
+  }
 
   @override
   void didChangeDependencies() async {
+    await _webSocketChannel?.ready;
+
     final dio = Dio();
     final stringJson = await CryptoInfoServiceImpl(dio).getCryptoInfo();
     List<dynamic> json = jsonDecode(stringJson);
@@ -53,8 +71,44 @@ class _ChartScreenState extends State<ChartScreen> {
       ),
       body: SizedBox.square(
         dimension: width,
-        child: Candlesticks(candles: _candles),
+        child: StreamBuilder(
+          stream: _webSocketChannel?.stream,
+          builder: (context, snapshot) {
+            if (snapshot.hasData && snapshot.data != null) {
+              final json = jsonDecode(snapshot.data)['k'];
+              if (json != null) {
+                final newestCandle = Candle(
+                  date: DateTime.fromMillisecondsSinceEpoch(json['t']),
+                  open: double.parse(json['o']),
+                  high: double.parse(json['h']),
+                  low: double.parse(json['l']),
+                  close: double.parse(json['c']),
+                  volume: double.parse(json['v']),
+                );
+
+                _candles[0] = newestCandle;
+
+                return Candlesticks(candles: _candles);
+              }
+            }
+
+            return SizedBox.shrink();
+          },
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _webSocketChannel?.sink.add(
+      jsonEncode({
+        "method": "UNSUBSCRIBE",
+        "params": ['ethusdt@kline_1d'],
+        "id": 1,
+      }),
+    );
+    _webSocketChannel?.sink.close();
+    super.dispose();
   }
 }
