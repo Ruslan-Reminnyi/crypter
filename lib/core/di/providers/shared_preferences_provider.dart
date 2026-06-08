2216a3b71b6edc8784eaaee4d9b08b253986f85f
Ruslan Reminnyi <reminny.r.v@gmail.com>
2026-06-08 10:09:34 +0300

    changing gitignore files

--- lib/core/di/providers/shared_preferences_provider.dart
+++ lib/core/di/providers/shared_preferences_provider.dart
@@ -2,4 +2,4 @@ part of 'package:crypter/core/di/providers/app_providers.dart';
 
 @riverpod
 SharedPreferences sharedPreferences(Ref _) =>
-    throw UnimplementedError('sharedPreferencesProvider must be initialized'); // to remember not forgetting to override the provider in AsyncAppDependencies
+    throw UnimplementedError('sharedPreferencesProvider must be initialized'); // remember to override the provider in AsyncAppDependencies
