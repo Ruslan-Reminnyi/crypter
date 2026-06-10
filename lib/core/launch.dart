ac0e6baa9cae2d3acc137cb3e71a4cab070b801b
Ruslan Reminnyi <reminny.r.v@gmail.com>
2026-06-10 13:04:29 +0300

    adding Firebase

---
+++ lib/core/launch.dart
@@ -0,0 +1,14 @@
+import 'package:crypter/core/di/async_app_dependencies.dart';
+import 'package:crypter/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<AsyncAppDependencies> launch() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  final asyncAppDependencies = await AsyncAppDependencies.init();
+
+  return asyncAppDependencies;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
-import 'package:crypter/core/di/async_app_dependencies.dart';
+import 'package:crypter/core/launch.dart';
 import 'package:crypter/presentation/features/app/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  final asyncAppDependencies = await AsyncAppDependencies.init();
+  final asyncAppDependencies = await launch();
 
   runApp(ProviderScope(overrides: asyncAppDependencies.overrides, child: const App()));
 }
