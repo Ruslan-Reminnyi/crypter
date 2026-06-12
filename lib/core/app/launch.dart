309cf3596befa30f23ebbe499e40625ef49a0c1e
Ruslan Reminnyi <reminny.r.v@gmail.com>
2026-06-13 01:32:50 +0300

    integrating Firebase

--- lib/core/app/launch.dart
+++ lib/core/app/launch.dart
@@ -1,5 +1,5 @@
 import 'package:crypter/core/di/async_app_dependencies.dart';
-import 'package:crypter/firebase_options.dart';
+import 'package:crypter/core/app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:crypter/core/launch.dart';
+import 'package:crypter/core/app/launch.dart';
 import 'package:crypter/presentation/features/app/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
